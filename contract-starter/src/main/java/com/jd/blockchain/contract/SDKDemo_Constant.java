5076b85692bbc1a969692c16fc59a27d58b7f247
zhaoguangwei <zhaoguangwei@jd.com>
2019-06-25 14:01:45 +0800

    基于jdchain1.0.0版本发布的新入门样例;

---
+++ contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
@@ -0,0 +1,38 @@
+package com.jd.blockchain.contract;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+
+public class SDKDemo_Constant {
+
+    public static final String GW_IPADDR = "211.151.11.178";
+
+    public static final int GW_PORT = 80;
+
+    public static final String[] PUB_KEYS = {
+            "3snPdw7i7PbUX47tXkHVuRd2mP8mXhEJbaBM441h9wa67TAeMfiqUX",
+            "3snPdw7i7PajLB35tEau1kmixc6ZrjLXgxwKbkv5bHhP7nT5dhD9eX",
+            "3snPdw7i7PZi6TStiyc6mzjprnNhgs2atSGNS8wPYzhbKaUWGFJt7x",
+            "3snPdw7i7PifPuRX7fu3jBjsb3rJRfDe9GtbDfvFJaJ4V4hHXQfhwk"};
+
+    public static final String[] PRIV_KEYS = {
+            "177gjuzAyvF8W2KYST8tVPmvGBsPLhXsf55HpHxSbHF7Va995ekXvxjNimEYNt5wP6GxTpW",
+            "177gju9p5zrNdHJVEQnEEKF4ZjDDYmAXyfG84V5RPGVc5xFfmtwnHA7j51nyNLUFffzz5UT",
+            "177gjtwLgmSx5v1hFb46ijh7L9kdbKUpJYqdKVf9afiEmAuLgo8Rck9yu5UuUcHknWJuWaF",
+            "177gk1pudweTq5zgJTh8y3ENCTwtSFsKyX7YnpuKPo7rKgCkCBXVXh5z2syaTCPEMbuWRns"};
+
+    public static final String PASSWORD = "abc";
+
+    public static final byte[] readChainCodes(String contractZip) {
+        // 构建合约的字节数组;
+        try {
+            ClassPathResource contractPath = new ClassPathResource(contractZip);
+            File contractFile = new File(contractPath.getURI());
+            return FileUtils.readFileToByteArray(contractFile);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
