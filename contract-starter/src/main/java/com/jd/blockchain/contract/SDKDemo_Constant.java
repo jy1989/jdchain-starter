75eec098e2da22b1974bcd1ba2f856dd691b9bf5
zhaoguangwei <zhaoguangwei@jd.com>
2019-06-25 14:41:58 +0800

    localhost link;

--- contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
+++ contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
@@ -7,18 +7,18 @@ import java.io.File;
 
 public class SDKDemo_Constant {
 
-    public static final String GW_IPADDR = "211.151.11.178";
+    public static final String GW_IPADDR = "127.0.0.1";
 
-    public static final int GW_PORT = 80;
+    public static final int GW_PORT = 11000;
 
     public static final String[] PUB_KEYS = {
-            "3snPdw7i7PbUX47tXkHVuRd2mP8mXhEJbaBM441h9wa67TAeMfiqUX",
+            "3snPdw7i7PjVKiTH2VnXZu5H8QmNaSXpnk4ei533jFpuifyjS5zzH9",
             "3snPdw7i7PajLB35tEau1kmixc6ZrjLXgxwKbkv5bHhP7nT5dhD9eX",
             "3snPdw7i7PZi6TStiyc6mzjprnNhgs2atSGNS8wPYzhbKaUWGFJt7x",
             "3snPdw7i7PifPuRX7fu3jBjsb3rJRfDe9GtbDfvFJaJ4V4hHXQfhwk"};
 
     public static final String[] PRIV_KEYS = {
-            "177gjuzAyvF8W2KYST8tVPmvGBsPLhXsf55HpHxSbHF7Va995ekXvxjNimEYNt5wP6GxTpW",
+            "177gjzHTznYdPgWqZrH43W3yp37onm74wYXT4v9FukpCHBrhRysBBZh7Pzdo5AMRyQGJD7x",
             "177gju9p5zrNdHJVEQnEEKF4ZjDDYmAXyfG84V5RPGVc5xFfmtwnHA7j51nyNLUFffzz5UT",
             "177gjtwLgmSx5v1hFb46ijh7L9kdbKUpJYqdKVf9afiEmAuLgo8Rck9yu5UuUcHknWJuWaF",
             "177gk1pudweTq5zgJTh8y3ENCTwtSFsKyX7YnpuKPo7rKgCkCBXVXh5z2syaTCPEMbuWRns"};
