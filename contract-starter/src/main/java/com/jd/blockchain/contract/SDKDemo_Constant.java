58cb3f5e1174f09989fa0a917878d2e6cb4dd11e
zhaoguangwei <zhaoguangwei@jd.com>
2019-08-16 17:58:14 +0800

    针对151.39的私钥上传;

--- contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
+++ contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
@@ -12,13 +12,13 @@ public class SDKDemo_Constant {
     public static final int GW_PORT = 18081;
 
     public static final String[] PUB_KEYS = {
-            "3snPdw7i7PfaSRzMdTbLFmbutQNCPoeMvPJaXzYHpw3VrXrsBhkC8m",
+            "3snPdw7i7PdLemquRBc1reMFBVA69JGAHXpk8ZnYvvXyUeaTSky9kC",
             "3snPdw7i7PajLB35tEau1kmixc6ZrjLXgxwKbkv5bHhP7nT5dhD9eX",
             "3snPdw7i7PZi6TStiyc6mzjprnNhgs2atSGNS8wPYzhbKaUWGFJt7x",
             "3snPdw7i7PifPuRX7fu3jBjsb3rJRfDe9GtbDfvFJaJ4V4hHXQfhwk"};
 
     public static final String[] PRIV_KEYS = {
-            "177gjubPJhZnyoNkAS2z63G4izmemjCxtuXDptMzMX9Esn7Fn3dkbFXN1SkJmvNrEtx9oQH",
+            "177gjuHvJznsVrfgGxF4A44MLKKo3VF2kyg91ukp6gzVDNZ1QgFgPZx3tbyJNiwtxPs8StU",
             "177gju9p5zrNdHJVEQnEEKF4ZjDDYmAXyfG84V5RPGVc5xFfmtwnHA7j51nyNLUFffzz5UT",
             "177gjtwLgmSx5v1hFb46ijh7L9kdbKUpJYqdKVf9afiEmAuLgo8Rck9yu5UuUcHknWJuWaF",
             "177gk1pudweTq5zgJTh8y3ENCTwtSFsKyX7YnpuKPo7rKgCkCBXVXh5z2syaTCPEMbuWRns"};
