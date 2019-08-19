74af1bb25370d8f75ddecae12549f2a069fee428
zhaoguangwei <zhaoguangwei@jd.com>
2019-08-19 19:11:07 +0800

    穿透式检索验证数据账户，分为两步来处理;

--- contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
+++ contract-starter/src/main/java/com/jd/blockchain/contract/SDKDemo_Constant.java
@@ -12,18 +12,18 @@ public class SDKDemo_Constant {
     public static final int GW_PORT = 18081;
 
     public static final String[] PUB_KEYS = {
-            "3snPdw7i7PdLemquRBc1reMFBVA69JGAHXpk8ZnYvvXyUeaTSky9kC",
+            "3snPdw7i7PbzpYs5kPQC4txDJGvkGrhSekic1rCmwd2u53ys9NDbu6",
             "3snPdw7i7PajLB35tEau1kmixc6ZrjLXgxwKbkv5bHhP7nT5dhD9eX",
             "3snPdw7i7PZi6TStiyc6mzjprnNhgs2atSGNS8wPYzhbKaUWGFJt7x",
             "3snPdw7i7PifPuRX7fu3jBjsb3rJRfDe9GtbDfvFJaJ4V4hHXQfhwk"};
 
     public static final String[] PRIV_KEYS = {
-            "177gjuHvJznsVrfgGxF4A44MLKKo3VF2kyg91ukp6gzVDNZ1QgFgPZx3tbyJNiwtxPs8StU",
+            "177gjyPd3WW3ZiegSJbQaz7a2vaDnYUrBu8EfFksmqtzAhMLYWs63T9xhGHaH2GncCGKfjV",
             "177gju9p5zrNdHJVEQnEEKF4ZjDDYmAXyfG84V5RPGVc5xFfmtwnHA7j51nyNLUFffzz5UT",
             "177gjtwLgmSx5v1hFb46ijh7L9kdbKUpJYqdKVf9afiEmAuLgo8Rck9yu5UuUcHknWJuWaF",
             "177gk1pudweTq5zgJTh8y3ENCTwtSFsKyX7YnpuKPo7rKgCkCBXVXh5z2syaTCPEMbuWRns"};
 
-    public static final String PASSWORD = "abc";
+    public static final String PASSWORD = "800000";
 
     public static final byte[] readChainCodes(String contractZip) {
         // 构建合约的字节数组;
